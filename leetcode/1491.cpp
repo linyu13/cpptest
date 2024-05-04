27df6f65f23bb6faec70a0837b2442c9ed9b7d23
linyu13 <3229515040@qq.com>
2024-05-04 12:08:08 +0800

    1491

---
+++ leetcode/1491.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    double average(vector<int>& salary) {
+        sort(salary.begin(), salary.end());
+        double sum = accumulate(salary.begin(), salary.end(), 0);
+        sum -= salary.front();
+        sum -= salary.back();
+        return sum / (salary.size() - 2) * 1.0;
+    }
+};
