f7b9da2dc059e9ce91771798f0a75814a9ad2da4
linyu13 <3229515040@qq.com>
2024-05-13 19:05:08 +0800

    1553

---
+++ leetcode/1553.cpp
@@ -0,0 +1,22 @@
+#include "leetcode.hpp"
+
+class Solution {
+private:
+    unordered_map<int, int> memo;
+
+public:
+    int minDays(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        if (memo.count(n)) {
+            return memo[n];
+        }
+        return memo[n] = min(n % 2 + 1 + minDays(n / 2), n % 3 + 1 + minDays(n / 3));
+    }
+};
+
+int main() {
+    Solution sol;
+    cout << sol.minDays(10) << "\n";
+}
