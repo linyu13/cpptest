dae3f73e28437a1cce6acab7531e8df03ea7ed41
linyu13 <3229515040@qq.com>
2024-05-22 17:21:12 +0800

    1881

---
+++ leetcode/1881.cpp
@@ -0,0 +1,37 @@
+#include "leetcode.hpp"
+
+class Solution {
+public:
+    string maxValue(string n, int x) {
+        int l = n.size();
+        if (n[0] == '-'){
+            for (int i = 1; i < l; ++i){
+                if (n[i] - '0' > x){
+                    n.insert(i, 1, '0' + x);
+                    return n;
+                }
+            }
+            n.push_back('0' + x);
+            return n;
+        }
+        else {
+            for (int i = 0; i < l; ++i){
+                if (n[i] - '0' < x){
+                    n.insert(i, 1, '0' + x);
+                    return n;
+                }
+            }
+            n.push_back('0' + x);
+            return n;
+        }
+
+    }
+};
+
+
+int main()
+{
+    string n = "-648468153646";
+    Solution sol;
+    cout << sol.maxValue(n, 5) << endl;
+}
